7c8e85f68acd97b6a4f365e08f5e97fab945990d
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-08-14 22:27:07 +0600

    database design and migration set for the project

---
+++ database/migrations/2021_08_14_164411_create_product_variants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductVariantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('size_id')->constrained('sizes');
+            $table->foreignId('color_id')->constrained('colors');
+            $table->float('variant_price', 8, 2);
+            $table->string('variant_image');
+            $table->integer('variant_qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_variants');
+    }
+}
